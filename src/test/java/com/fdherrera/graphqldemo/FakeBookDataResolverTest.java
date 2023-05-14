7de13cb76209e9b569af697c2af35dd71fce09ba
FdHerrera <soyfdherrera@gmail.com>
2023-05-14 16:50:47 -0300

    Adds testing for finding by release

--- src/test/java/com/fdherrera/graphqldemo/FakeBookDataResolverTest.java
+++ src/test/java/com/fdherrera/graphqldemo/FakeBookDataResolverTest.java
@@ -2,16 +2,24 @@ package com.fdherrera.graphqldemo;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.fdherrera.graphqldemo.component.fake.FakeBookDataResolver;
+import com.fdherrera.graphqldemo.datasource.fake.FakeBookDataSource;
+import com.fdherrera.graphqldemo.generated.client.BooksByReleaseGraphQLQuery;
+import com.fdherrera.graphqldemo.generated.client.BooksProjectionRoot;
+import com.fdherrera.graphqldemo.generated.client.Books_ReleasedProjection;
 import com.fdherrera.graphqldemo.generated.types.Author;
 import com.fdherrera.graphqldemo.generated.types.Book;
 import com.fdherrera.graphqldemo.generated.types.ReleaseHistory;
+import com.fdherrera.graphqldemo.generated.types.ReleaseHistoryInput;
 import com.netflix.graphql.dgs.DgsQueryExecutor;
+import com.netflix.graphql.dgs.client.codegen.GraphQLQueryRequest;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
+import java.util.concurrent.ThreadLocalRandom;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,6 +33,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class FakeBookDataResolverTest {
     @Autowired private DgsQueryExecutor queryExecutor;
+    @Autowired private FakeBookDataSource dataSource;
 
     @Test
     void shouldReturnAllBooksWithNonNullDataForSpecificFieldsInGraph() {
@@ -74,6 +83,28 @@ class FakeBookDataResolverTest {
                 ReleaseHistory actualRelease = book.getReleased();
                 assertNotNull(actualRelease.getYear());
             }
+        });
+    }
+
+    @Test
+    void shouldReturnBooksFilteredByRelease() {
+        Book someBook = dataSource.getBooks().get(ThreadLocalRandom.current().nextInt(0, 19));
+        Boolean expectedHasPrintedEdition = someBook.getReleased().getPrintedEdition();
+        Integer expectedReleaseYear = someBook.getReleased().getYear();
+        ReleaseHistoryInput releaseQueryInput = ReleaseHistoryInput.newBuilder().year(expectedReleaseYear).hasPrintedEdition(expectedHasPrintedEdition).build();
+        BooksByReleaseGraphQLQuery input = BooksByReleaseGraphQLQuery.newRequest().releasedInput(releaseQueryInput).build();
+        Books_ReleasedProjection projection = new BooksProjectionRoot().released().year().printedEdition();
+
+        String graphqlRequest = new GraphQLQueryRequest(input, projection).serialize();
+
+        Book[] booksArray = queryExecutor.executeAndExtractJsonPathAsObject(graphqlRequest, "data.booksByRelease", Book[].class);
+        List<Book> actualBooks = Arrays.asList(booksArray);
+
+        assertNotNull(actualBooks);
+        assertTrue(actualBooks.size() <= 1);
+        actualBooks.forEach(book -> {
+            assertNotNull(book.getReleased().getYear());
+            assertNotNull(book.getReleased().getPrintedEdition());
         });
     }
 }
