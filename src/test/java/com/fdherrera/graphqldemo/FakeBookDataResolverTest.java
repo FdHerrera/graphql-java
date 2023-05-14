6b12887314cf9a96d4a110fceca5f71cf1473178
FdHerrera <soyfdherrera@gmail.com>
2023-05-14 13:40:13 -0300

    Extends assertions in books test

--- src/test/java/com/fdherrera/graphqldemo/FakeBookDataResolverTest.java
+++ src/test/java/com/fdherrera/graphqldemo/FakeBookDataResolverTest.java
@@ -4,9 +4,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import com.fdherrera.graphqldemo.component.fake.FakeBookDataResolver;
+import com.fdherrera.graphqldemo.generated.types.Author;
 import com.fdherrera.graphqldemo.generated.types.Book;
+import com.fdherrera.graphqldemo.generated.types.ReleaseHistory;
 import com.netflix.graphql.dgs.DgsQueryExecutor;
+
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -26,9 +32,16 @@ class FakeBookDataResolverTest {
         {
             books(author : "") {
                 title 
+                publisher
                 author{
                     name 
                     originCountry
+                    addresses {
+                        street
+                        city
+                        country
+                        zipcode
+                    }
                 } 
                 released {
                     year 
@@ -37,10 +50,30 @@ class FakeBookDataResolverTest {
                 }
             }
         }
-        """;
+        """;  
 
-        List<Book> actualBooks = queryExecutor.executeAndExtractJsonPath(query, "data.books");
+        Book[] booksArray = queryExecutor.executeAndExtractJsonPathAsObject(query, "data.books", Book[].class);
+        List<Book> actualBooks = Arrays.asList(booksArray);
         assertNotNull(actualBooks);
         assertEquals(20, actualBooks.size());
+        actualBooks.forEach(book -> {
+            assertNotNull(book);
+            assertNotNull(book.getTitle());
+            assertNotNull(book.getPublisher());
+            assertNotNull(book.getAuthor());
+            Author author = book.getAuthor();
+            assertNotNull(author.getName());
+            assertNotNull(author.getOriginCountry());
+            assertNotNull(author.getAddresses());
+            author.getAddresses().forEach(address -> {
+                assertNotNull(address.getStreet());
+                assertNotNull(address.getCity());
+                assertNotNull(address.getCountry());
+            });
+            if (!Objects.isNull(book.getReleased())) {
+                ReleaseHistory actualRelease = book.getReleased();
+                assertNotNull(actualRelease.getYear());
+            }
+        });
     }
 }
