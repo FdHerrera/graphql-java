3f1b991ec55f75fef667161412c291960222fd78
FdHerrera <soyfdherrera@gmail.com>
2023-05-14 23:01:58 -0300

    Extends FakeMobileAppDataSource

--- src/main/java/com/fdherrera/graphqldemo/datasource/fake/FakeMobileAppDataSource.java
+++ src/main/java/com/fdherrera/graphqldemo/datasource/fake/FakeMobileAppDataSource.java
@@ -1,19 +1,19 @@
 package com.fdherrera.graphqldemo.datasource.fake;
 
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-
-import org.springframework.stereotype.Component;
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toSet;
 
 import com.fdherrera.graphqldemo.generated.types.MobileApp;
-
 import jakarta.annotation.PostConstruct;
+import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.IntStream;
 import net.datafaker.Faker;
+import org.springframework.stereotype.Component;
 
 @Component
 public class FakeMobileAppDataSource extends CommonFakeSource {
-
     public FakeMobileAppDataSource(Faker faker) {
         super(faker);
     }
@@ -22,12 +22,37 @@ public class FakeMobileAppDataSource extends CommonFakeSource {
 
     @PostConstruct
     private void postConstruct() {
-        mobileApps = IntStream.range(0, 20)
-            .mapToObj(i -> buildMobileApp())
-            .collect(Collectors.toList());
+        mobileApps = IntStream.range(0, 20).mapToObj(i -> buildMobileApp()).collect(toList());
+    }
+
+    public List<MobileApp> getMobileApps() {
+        return mobileApps;
     }
 
     private MobileApp buildMobileApp() {
-        return null;
+        return MobileApp.newBuilder()
+            .name(faker.name().name())
+            .version(faker.app().version())
+            .platform(getPlatforms())
+            .author(buildAuthor())
+            .build();
+    }
+
+    private List<String> getPlatforms() {
+        return IntStream.range(0, 3)
+            .mapToObj(i -> {
+                if (ThreadLocalRandom.current().nextBoolean()) {
+                    return List.of("Windows", "IOs", "Ubuntu", "CentOS", "Fedora", "Debian", "Mint")
+                        .stream()
+                        .findAny()
+                        .orElse(null);
+                } else {
+                    return null;
+                }
+            })
+            .filter(s -> !Objects.isNull(s))
+            .collect(toSet())
+            .stream()
+            .collect(toList());
     }
 }
